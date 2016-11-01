f4f093935842b907f9444079301135507f65caf8
Pedro Costa <pedro.mpm.costa@gmail.com>
2016-11-01 22:41:14 +0000

    minor changes

--- get_plan.m
+++ get_plan.m
@@ -3,11 +3,12 @@
 % returns: the optimal plan as a ordinal array
 function y = get_plan()
     global HRZN TRix CHix;
-    optimal = ones(1,HRZN);
+    
+    optimal = -ones(1,HRZN);        % initialize plan
     x = 1;
     for i=1:HRZN
-        optimal(i) = CHix(i,x);
-        x = TRix(i,x);
+        optimal(i) = CHix(i,x);     % store current optimal decision
+        x = TRix(i,x);              % get next state
     end
     y=ordinal(optimal,{'keep';'replace'});
 end
